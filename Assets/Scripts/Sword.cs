c9ed1a814871b1e85bcaa45639eb0f4f23f716a4
PavleMomcilovic <163585769+PavleMomcilovic@users.noreply.github.com>
2026-04-10 18:32:50 +0200

    Sword follow
    dodata animacija da mac prati polozaj misa na ekranu

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    // deo za napad
     private PlayerControls playerControls;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    // deo za animacije
     private Animator myAnimator;
 
     private void Awake() {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+
         myAnimator = GetComponent<Animator>();
+
         playerControls = new PlayerControls();
     }
 
@@ -19,7 +28,23 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update() {
+        MouseFollowWithOffset();
+    }
+
     private void Attack() {
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerPos = Camera.main.WorldToScreenPoint(playerController.transform.position);
+        
+        float angle = Mathf.Atan2(mousePos.y - playerPos.y, Mathf.Abs(mousePos.x - playerPos.x)) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerPos.x)
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        else
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
